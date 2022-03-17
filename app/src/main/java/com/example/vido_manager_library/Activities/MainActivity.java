6289e9040c0cb14c76db3c2fd56fbd70a4ab8eff
ASUS <nhattiennguyen224@gmail.com>
2022-03-17 14:12:50 +0700

    Initial Commit

--- app/src/main/java/com/example/vido_manager_library/Activities/MainActivity.java
+++ app/src/main/java/com/example/vido_manager_library/Activities/MainActivity.java
@@ -9,6 +9,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.vido_manager_library.Activities.AboutActivity;
 import com.example.vido_manager_library.Activities.BooksActivity;
@@ -16,6 +18,7 @@ import com.example.vido_manager_library.Activities.BorrowBooksActivity;
 import com.example.vido_manager_library.Adapters.BooksAdapters;
 import com.example.vido_manager_library.Models.HomeHorModels;
 import com.example.vido_manager_library.R;
+import com.example.vido_manager_library.UserDetailActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
@@ -26,11 +29,24 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView homeHorizontalRec, homeHorizontalRec2;
     List<HomeHorModels> bookList;
     BooksAdapters booksAdapters;
+    ImageView user_detail_circle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //detail user - thong tin sinh vien
+        user_detail_circle = findViewById(R.id.user_circle);
+
+        user_detail_circle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, UserDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //cardView home
         homeHorizontalRec = findViewById(R.id.home_hor_rec);
         homeHorizontalRec2 = findViewById(R.id.home_hor_rec2);
         bookList = new ArrayList<>();
@@ -42,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         bookList.add(new HomeHorModels(R.drawable.logo, "CDVD Book"));
         bookList.add(new HomeHorModels(R.drawable.logo, "CDVD Book"));
 
-        booksAdapters = new BooksAdapters(getApplication(), bookList);
+        booksAdapters = new BooksAdapters(this, bookList);//context to activity => getApplication() to this
         //Sách chuyên ngành
         homeHorizontalRec.setAdapter(booksAdapters);
         homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getApplication(), RecyclerView.HORIZONTAL, false));
--- app/src/main/java/com/example/vido_manager_library/Adapters/BooksAdapters.java
+++ app/src/main/java/com/example/vido_manager_library/Adapters/BooksAdapters.java
@@ -1,5 +1,6 @@
 package com.example.vido_manager_library.Adapters;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,11 +18,13 @@ import java.util.List;
 
 public class BooksAdapters extends RecyclerView.Adapter<BooksAdapters.ViewHolder> {
 
-    Context context;
+    //Context context;
+    Activity activity;
     List<HomeHorModels> list;
 
-    public BooksAdapters(Context context, List<HomeHorModels> list) {
-        this.context = context;
+    //Context to Activity
+    public BooksAdapters(Activity activity, List<HomeHorModels> list) {
+        this.activity = activity;
         this.list = list;
     }
 
