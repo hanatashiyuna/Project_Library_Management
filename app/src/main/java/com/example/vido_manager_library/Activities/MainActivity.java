a1645990cbd9a847438517edea225030d995bb5d
ASUS <nhattiennguyen224@gmail.com>
2022-03-18 08:52:52 +0700

    Initial Commit

--- app/src/main/java/com/example/vido_manager_library/Activities/MainActivity.java
+++ app/src/main/java/com/example/vido_manager_library/Activities/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.vido_manager_library.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.vido_manager_library.Adapters.BooksAdapters;
+import com.example.vido_manager_library.Models.Capture;
 import com.example.vido_manager_library.Models.HomeHorModels;
 import com.example.vido_manager_library.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,10 +34,33 @@ public class MainActivity extends AppCompatActivity {
     List<HomeHorModels> bookList;
     BooksAdapters booksAdapters;
     ImageView user_detail_circle;
+    FloatingActionButton scanner_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //scanner
+        scanner_btn = findViewById(R.id.fab);
+
+        scanner_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //khoi tao intent
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                //Set prompt text
+                intentIntegrator.setPrompt("For flash use volume up key");
+                //Set beep
+                intentIntegrator.setBeepEnabled(true);
+                //locked oritation
+                intentIntegrator.setOrientationLocked(true);
+                //set capture activity
+                intentIntegrator.setCaptureActivity(Capture.class);
+                //initiate scan
+                intentIntegrator.initiateScan();
+            }
+        });
 
         //detail user - thong tin sinh vien
         user_detail_circle = findViewById(R.id.user_circle);
@@ -95,5 +126,35 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    //scanner
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        //check condition
+        if(intentResult.getContents() != null){
+            /**
+             * khi content = null: khoi tao alert dialog
+             */
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Kết quả");
+            builder.setMessage(intentResult.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            builder.show();
+        }else{
+            /**
+             * khi content ko null: in ra 1 doan hoi thoai
+             */
+            Toast.makeText(getApplication(), "Bạn vẫn chưa scan thứ gì...", Toast.LENGTH_SHORT).show();
+        }
     }
 }
