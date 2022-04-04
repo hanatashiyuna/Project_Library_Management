1325541acab073647a5db79346867989980d199b
ASUS <nhattiennguyen224@gmail.com>
2022-04-04 09:57:41 +0700

    Yuna Save 0404

---
+++ app/src/main/java/com/example/vido_manager_library/Interface/IClickItemCategory.java
@@ -0,0 +1,7 @@
+package com.example.vido_manager_library.Interface;
+
+import com.example.vido_manager_library.Models.Categorys;
+
+public interface IClickItemCategory {
+    public void onClickItemCategory(Categorys categorys);
+}
